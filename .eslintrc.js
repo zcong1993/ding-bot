b6bd8c8072c8a06a7f54b26cc07019203aaf0c7b
zcong1993 <zhangcong1992@gmail.com>
2020-06-19 16:54:16 +0800

    fix: disable @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     // '@typescript-eslint/no-use-before-define': 'off',
     // '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 }
